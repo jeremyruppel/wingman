1c40ef0621b735886ac62206b57b72490c88da55
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2012-02-22 22:01:25 -0800

    Refactor stats and tests into own specs

--- spec/unit/config_spec.rb
+++ spec/unit/config_spec.rb
@@ -19,47 +19,5 @@ describe Wingman do
     end
 
     it { should respond_to( :reset! ) }
-
-    context '#test' do
-
-      it { should respond_to( :test ) }
-
-      context 'the test array' do
-        subject { Wingman.tests }
-
-        it { should be_kind_of( Array ) }
-      end
-
-      it 'should add a new test to the test array' do
-        Wingman.configure do |config|
-          config.test :foo do
-            'bar'
-          end
-        end
-
-        Wingman.tests.should have( 1 ).item
-      end
-    end
-
-    context '#stat' do
-
-      it { should respond_to( :stat ) }
-
-      context 'the stat array' do
-        subject { Wingman.stats }
-
-        it { should be_kind_of( Array ) }
-      end
-
-      it 'should add a new stat to the stat array' do
-        Wingman.configure do |config|
-          config.stat :foo do
-            'bar'
-          end
-        end
-
-        Wingman.stats.should have( 1 ).item
-      end
-    end
   end
 end
